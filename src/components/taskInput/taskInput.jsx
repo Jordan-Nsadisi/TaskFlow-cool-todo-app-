bdaf92e2060dcbf5259fa44b5c37b491014ad500
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-14 02:31:49 +0100

    ecoute de l'évenement au changement de l'input mis a jour de la valeur de son etat

--- src/components/taskInput/taskInput.jsx
+++ src/components/taskInput/taskInput.jsx
@@ -1,7 +1,16 @@
 //ce composant est utiliser pour afficher le champ de saisie des tâches
+import { useState } from "react";
 import styles from "./taskInput.module.css"
 
 export const TaskInput = () => {
+
+    const [taskTitle, setTaskTitle] = useState("") //hook pour la gestion de l'etat du titre du task
+
+    const handleInputChange = (event) => { //fonctionner qui permet de capturer ce qui est ecrit dans l'input du formulaire, elle prend en paramètre l'èvenement au changement de l'input
+        console.log(event.target.value);
+        setTaskTitle(event.target.value) //met à jour la valeur du titre du task(tâche)
+    }
+
     return (
         <div className={`box ${styles.element}`}>
             <h2 className={styles.title}>🎯 Ajoute ta prochaine tâche</h2>
@@ -10,6 +19,7 @@ export const TaskInput = () => {
                     type="text"
                     className={styles.input}
                     placeholder="Indiquez un titre de place explicite."
+                    onChange={handleInputChange}
                 />
                 <button className="button-primary">Ajouter</button>
             </form>
