6f6bdc7e16c5e68757200a36595b57162ad45dce
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-16 00:14:12 +0100

    conditionnal return of footer components

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,17 @@
 import styles from "./footer.module.css"
 
 export const Footer = ({ completedTasks }) => {
-    return (
-        <footer>
-            <code className={styles.footer}>
-                Avec TaskFlow, tu as éliminé {completedTasks} tâches
-            </code>
-        </footer>
-    )
+
+    if (completedTasks) { //si les tâches complétés existent
+        return (
+            <footer>
+                <code className={styles.footer}>
+                    Avec TaskFlow, tu as éliminé {completedTasks} tâches
+                </code>
+            </footer>
+        )
+    }
+
+    return null
+    //return <></>
 }
