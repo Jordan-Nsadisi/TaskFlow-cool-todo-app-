9402f8725f7149c62579781f3dd6207f2f55c801
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-15 22:12:52 +0100

    parcour et rendue de la liste des tâches dans les taskItem

--- src/components/taskList/taskList.jsx
+++ src/components/taskList/taskList.jsx
@@ -7,14 +7,22 @@ export const TaskLists = (
     { tasksList, incompletedTasks, editTask, deleteTask } //recupère en props les outils crée et transmis depuis le composant parent.
 ) => {
 
+    const taskLists = tasksList.map((task) => ( //externalisation du pacours du tableau de taches pour les afficher à chaque tour dans un composant taskItem
+        <TaskItem key={task.id} />
+    ))
+
     return (
         <div className='box'>
             <h2 className={StyleSheet.title}>
                 il te reste encore {incompletedTasks} tâches à accomplir !
             </h2>
-            <ul className={styles.container}>
-                <TaskItem />
-            </ul>
+
+            {tasksList && tasksList.lenght > 0 && ( // si le tableau des tâches existe et le nombre des tâches est superieur à 0, ...
+                <ul className={styles.container}>
+                    {taskLists} {/* on affiche dans une liste, le tableau mapper dans un composant TaskItem */}
+                </ul>
+            )}
+
         </div>
     )
 }
