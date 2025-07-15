211c777336c79ac98882362249aeabe655b8fea4
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-15 22:31:14 +0100

    affichage des données, edition et suppression des chasue tâches

--- src/components/taskItem/taskItem.jsx
+++ src/components/taskItem/taskItem.jsx
@@ -2,19 +2,22 @@
 import styles from "./taskItem.module.css"
 
 
-const TaskItem = ({ task, editTask, deleteTask }) => { //props drilling depuis le parent le plus haut 'TaskContainer'
+const TaskItem = ({ task, editTask, deleteTask }) => { //props drilling depuis le parent le plus haut 'TaskContainer', sauf 'task' qui est l'objet contenant les infos de chaque tâche, mappé deouis le composant TaskList
     return (
-        <li className={`${styles.container} ${styles.default}`}>
+        <li
+            className={`${styles.container} ${styles.default}`}
+            onClick={() => editTask(task.id, !task.isCompleted)} //on joue la fonction d'edition et on lui passe l'id de la tache et son status different à chaque clique, en gros on change le statut complété de la tache, à l'évenement au clique
+        >
             <div className={styles.item}>
                 <div className={`${styles.id} ${styles.idDefault}`}>
-                    1
+                    {task.id}
                 </div>
                 <div className={styles.contentDefault}>
-                    titre : finire d'apprendre react
+                    {task.title}
                 </div>
             </div>
             <button className="button-primary">
-                x
+                {deleteTask} {/* pour supprimer un tâche */}
             </button>
         </li>
     )
