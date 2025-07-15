48d16307d2c99d2502de58c0e1f666937b875a49
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-15 23:04:48 +0100

    conditionnal rendering of component TaskItem

--- src/components/taskItem/taskItem.jsx
+++ src/components/taskItem/taskItem.jsx
@@ -5,19 +5,19 @@ import styles from "./taskItem.module.css"
 const TaskItem = ({ task, editTask, deleteTask }) => { //props drilling depuis le parent le plus haut 'TaskContainer', sauf 'task' qui est l'objet contenant les infos de chaque tâche, mappé deouis le composant TaskList
     return (
         <li
-            className={`${styles.container} ${styles.default}`}
+            className={`${styles.container} ${task?.isCompleted ? styles.success : styles.default}`}
             onClick={() => editTask(task.id, !task.isCompleted)} //on joue la fonction d'edition et on lui passe l'id de la tache et son status different à chaque clique, en gros on change le statut complété de la tache, à l'évenement au clique
         >
             <div className={styles.item}>
-                <div className={`${styles.id} ${styles.idDefault}`}>
+                <div className={`${styles.id} ${task?.isCompleted ? styles.idSuccess : styles.idDefault}`}>
                     {task.id}
                 </div>
-                <div className={styles.contentDefault}>
+                <div className={task?.isCompleted ? styles.contentSuccess : styles.contentDefault}>
                     {task.title}
                 </div>
             </div>
             <button className="button-primary">
-                {deleteTask} {/* pour supprimer un tâche */}
+                supprimer un tâche
             </button>
         </li>
     )
