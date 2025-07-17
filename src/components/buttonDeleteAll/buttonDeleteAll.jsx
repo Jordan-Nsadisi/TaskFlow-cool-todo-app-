65764dd6c482ee84ce802035326b6338321d0b51
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-17 01:12:24 +0100

    add animated of icon

--- src/components/buttonDeleteAll/buttonDeleteAll.jsx
+++ src/components/buttonDeleteAll/buttonDeleteAll.jsx
@@ -12,16 +12,27 @@ export const ButtonDeleteAll = ({ deleteAllTask, tasksList }) => {
         deleteAllTask()
     }
 
+    const playerRef = useRef(null); // création d'une référence pour contrôler l'animation Lordicon, definie par defaut à null pour éviter les erreurs de rendu avant que le composant soit monté
+    const handlePlay = () => { // fonction pour jouer l’animation au survol
+        playerRef.current?.playFromBeginning(); // Utilisation de l’opérateur de chaînage optionnel pour éviter les erreurs si playerRef.current est null
+    };
+    const handleStop = () => { // fonction pour arrêter l’animation quand la souris quitte
+        playerRef.current?.stop();
+    };
+
+
     if (tasksList.length > 0) { //si la valeur des éléments du tableau est supperieur à 0
         return (
             <button
                 className="button-delete-all"
                 onClick={handleDeleteAllTask}
+                onMouseEnter={handlePlay} //on joue l'animation au survol de la souris
+                onMouseLeave={handleStop}
             >
                 <Player
                     icon={deleteAllicon}
-                    size={21}
-                    colorize="#2870ff"
+                    size={24}
+                    colorize="#ff2828"
                     loop={false} //ne boucle pas l’animation
                     ref={playerRef} //on passe la référence pour contrôler l'animation
                 />
