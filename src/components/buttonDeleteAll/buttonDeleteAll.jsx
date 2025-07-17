735d4e79ecf8bbb648134b031b8c87b96cc123cf
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-17 03:49:16 +0100

    add sound on button deletAll tasks

--- src/components/buttonDeleteAll/buttonDeleteAll.jsx
+++ src/components/buttonDeleteAll/buttonDeleteAll.jsx
@@ -3,12 +3,14 @@ import { Player } from '@lordicon/react' //importation de la librairie pour les 
 import deleteAllicon from '../../assets/deleteAllicon.json' //importation de l'icone de suppression
 import { useRef } from 'react' // Import du hook useRef pour contrôler l’animation
 import { Tooltip } from 'react-tooltip'; // Importation de ReactTooltip pour personalier les infobulles
+import useSound from 'use-sound'; // Importation du hook useSound pour jouer un son lors de l'interaction avec le bouton, importer depuis la librairie use-sound
+import trashAll from '../../assets/sounds/trashAll.mp3'; // Importation du son de la corbeille
 
 
 export const ButtonDeleteAll = ({ deleteAllTask, tasksList }) => {
 
-    const handleDeleteAllTask = (event) => { //fonction de suppression de toutes les tasks
-        event.preventDefault(); //empêche le comportement par défaut du formulaire qui consiste à recharger la page
+    const handleDeleteAllTask = () => { //fonction de suppression de toutes les tasks. Pas besoin de capter l'évenement vue que le bouton n'est pas pas dans un formulaire
+        // event.preventDefault(); //empêche le comportement par défaut du formulaire qui consiste à recharger la page
         deleteAllTask()
     }
 
@@ -17,12 +19,17 @@ export const ButtonDeleteAll = ({ deleteAllTask, tasksList }) => {
         playerRef.current?.playFromBeginning(); // Utilisation de l’opérateur de chaînage optionnel pour éviter les erreurs si playerRef.current est null
     };
 
+    const [playSound] = useSound(trashAll, { volume: 0.1 }) // Utilisation du hook useSound pour jouer le son de la corbeille, avec un volume de 0.5
+
 
     if (tasksList.length > 0) { //si la valeur des éléments du tableau est supperieur à 0
         return (<>
             <button
                 className="button-delete-all"
-                onClick={handleDeleteAllTask}
+                onClick={() => {
+                    handleDeleteAllTask();
+                    playSound(); // Joue le son au clic
+                }} //onClick pour supprimer toutes les tâches et jouer le son
                 data-tooltip-id="deleteAllTooltip" //id de l'infobulle
                 data-tooltip-content="Supprimer toutes les tâches" //contenu de l'infobulle
                 onMouseEnter={handlePlay} //on joue l'animation au survol de la souris
