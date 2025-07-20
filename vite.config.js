146bf317d4188968c6d499dd62206b18d14cc205
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-07-20 03:13:38 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // désactive la minification pour voir les erreurs plus lisiblement
+    sourcemap: true, // génère des sourcemaps pour faciliter le débogage
+  }
 })
